03b4d257ae4d2ba4e1d4f8fa53e5794bbb2f4a4e
Renan Couto <renancouto@gmail.com>
2014-05-28 08:15:29 -0300

    Rewrite of dictionary method

--- test/test.js
+++ test/test.js
@@ -6,16 +6,18 @@ var assert      = require('assert');
 var should      = require('should');
 var wordSuggest = require('../');
 var dictionary  = require('../lib/dictionary');
+var fixtures    = require('./fixtures');
 
 describe('Word Suggest', function () {
-    it('should return a word with 5 letters', function () {
-        assert.equal(5, wordSuggest.length);
+    it('should load lo-RM dictionary', function () {
+        assert.deepEqual(dictionary('lo-RM'), fixtures.dictionary);
     });
 
-    it('should have at least lo-RM dictionary', function (done) {
-        dictionary(function (err, results) {
-            results.should.have.property('lo-RM');
-            done();
-        });
+    it('should return a word with 5 letters', function () {
+        assert.equal(5, wordSuggest('lorem', 'lo-RM').length);
+    });
+
+    it('should check if the word has underscores', function () {
+        assert.equal(true, wordSuggest('lo_em', 'lo-RM'));
     });
 });
