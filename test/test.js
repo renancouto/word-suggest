123c37e818292c384234e0605afe51308368128b
Renan Couto <renancouto@gmail.com>
2014-05-27 22:23:15 -0300

    Added sample dictionary

--- test/test.js
+++ test/test.js
@@ -1,12 +1,21 @@
-/*jslint node:true*/
+/*jslint node:true, nomen:true, unparam:true*/
 /*global describe, it*/
 'use strict';
 
 var assert      = require('assert');
+var should      = require('should');
 var wordSuggest = require('../');
+var dictionary  = require('../lib/dictionary');
 
 describe('Word Suggest', function () {
     it('should return a word with 5 letters', function () {
         assert.equal(5, wordSuggest.length);
+    });
+
+    it('should have at least lo-RM dictionary', function (done) {
+        dictionary(function (err, results) {
+            results.should.have.property('lo-RM');
+            done();
+        });
     });
 });
